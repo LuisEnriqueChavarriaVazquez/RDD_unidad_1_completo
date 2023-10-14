c0c5fe35dcae7b7938ca62c2e6439a8872131912
UlisesJuarez <uje170399@gmail.com>
2023-10-14 16:11:00 -0600

    disclaimer para el usuario en evalucion

---
+++ js/evaluacion/disclaimer_user.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modal = document.getElementById('modalDisclaimer');
+
+    var bootstrapModal = new bootstrap.Modal(modal);
+    bootstrapModal.show();
+});
