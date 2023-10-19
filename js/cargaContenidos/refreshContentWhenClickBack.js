e582123973212e27d965f97de6a7b02c121d6c6f
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-10-19 16:13:44 -0600

    Solucion a bug de error en la carga cuando retrocedemos

---
+++ js/cargaContenidos/refreshContentWhenClickBack.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtiene la URL actual del navegador
+    const url = window.location.href;
+
+    // Divide la URL por el carácter "/" y toma el último elemento
+    const partes = url.split('/');
+    const textoDespuesDelUltimoSlash = partes[partes.length - 1];
+
+    // Verifica si urlActualRetorno existe y no es 'unidad1.html' en el Local Storage
+    const urlActualRetorno = localStorage.getItem('urlActualRetorno');
+    
+    if (urlActualRetorno === null || urlActualRetorno !== 'unidad1.html') {
+        // Si urlActualRetorno no existe o es diferente de 'unidad1.html', guárdalo y recarga la página
+        localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
+        //alert('reload')
+        location.reload();
+    } else {
+        // Si urlActualRetorno es igual a 'unidad1.html', guárdalo en el Local Storage
+        localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
+    }
+});
