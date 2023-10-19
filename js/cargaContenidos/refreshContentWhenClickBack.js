011442a7fab56919a24223d63f988a5fd6d98ab6
LuisEnriqueChavarriaVazquez <cuentamovilapps287932@gmail.com>
2023-10-19 16:30:26 -0600

    Solucion a bug de error en la carga cuando retrocedemos

--- js/cargaContenidos/refreshContentWhenClickBack.js
+++ js/cargaContenidos/refreshContentWhenClickBack.js
@@ -1,25 +1,25 @@
-setTimeout( () => {
-    // Obtiene la URL actual del navegador
-    const url = window.location.href;
+// setTimeout( () => {
+//     // Obtiene la URL actual del navegador
+//     const url = window.location.href;
     
-    // Divide la URL por el carácter "/" y toma el último elemento
-    const partes = url.split('/');
-    const textoDespuesDelUltimoSlash = partes[partes.length - 1];
+//     // Divide la URL por el carácter "/" y toma el último elemento
+//     const partes = url.split('/');
+//     const textoDespuesDelUltimoSlash = partes[partes.length - 1];
     
-    // Verifica si urlActualRetorno existe y no es 'unidad1.html' en el Local Storage
-    const urlActualRetorno = localStorage.getItem('urlActualRetorno');
+//     // Verifica si urlActualRetorno existe y no es 'unidad1.html' en el Local Storage
+//     const urlActualRetorno = localStorage.getItem('urlActualRetorno');
     
-    if (urlActualRetorno === null || urlActualRetorno !== 'unidad1.html') {
-        // Si urlActualRetorno no existe o es diferente de 'unidad1.html', guárdalo y recarga la página
-        localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
-        //alert('reload')
-        location.reload();
-    } else {
-        // Si urlActualRetorno es igual a 'unidad1.html', guárdalo en el Local Storage
-        localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
-    }
-}, 1000);
+//     if (urlActualRetorno === null || urlActualRetorno !== 'unidad1.html') {
+//         // Si urlActualRetorno no existe o es diferente de 'unidad1.html', guárdalo y recarga la página
+//         localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
+//         //alert('reload')
+//         location.reload();
+//     } else {
+//         // Si urlActualRetorno es igual a 'unidad1.html', guárdalo en el Local Storage
+//         localStorage.setItem('urlActualRetorno', textoDespuesDelUltimoSlash);
+//     }
+// }, 1000);
 
-window.addEventListener('popstate', function(event) {
-    console.log("Botón de retroceso/avance clickeado!");
-});
+// window.addEventListener('popstate', function(event) {
+//     console.log("Botón de retroceso/avance clickeado!");
+// });
